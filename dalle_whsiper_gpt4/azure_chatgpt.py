7fcba0868ff8a91476117516486702e6c77142fe
gov-source <52269410+gov-source@users.noreply.github.com>
2024-12-19 15:31:35 +0000

    added semantic kernels

--- dalle_whsiper_gpt4/azure_chatgpt.py
+++ dalle_whsiper_gpt4/azure_chatgpt.py
@@ -1,31 +1,35 @@
 import os
-import openai
+from openai import AzureOpenAI
+from dotenv import load_dotenv
+from os.path import dirname
 
-openai.api_type = "azure"
-openai.api_base = "https://coursera-azure-openai-service.openai.azure.com/"
-openai.api_version = "2024-02-15-preview"
-openai.api_key = "D68tqjlf1v9kwYmcLyRfzIbbSErkJHBPNinuew6m1HZafg5pOksGJQQJ99ALACYeBjFXJ3w3AAABACOGeUCa"
+current_dir = dirname(os.path.abspath(__file__))
+root_dir = dirname(dirname(current_dir))
+env_file = os.path.join(root_dir, '.env')
+load_dotenv(env_file)
+
+client = AzureOpenAI(azure_endpoint=os.environ["OPENAI_API_BASE"],
+api_version=os.environ["OPENAI_API_VERSION"],
+api_key=os.environ["OPENAI_API_KEY"])
 
 def ai_chat(user_message):
     message_text = [
         {"role": "system", "content": "You are an AI assistant that helps people find information."},
         {"role": "user", "content": user_message}
     ]
-    
-    completion = openai.ChatCompletion.create(
-        engine="gpt-4o-mini",
-        messages=message_text,
-        temperature=1,
-        max_tokens=800,
-        top_p=0.95,
-        frequency_penalty=0,
-        presence_penalty=0,
-        stop=None
-    )
+
+    completion = client.chat.completions.create(model="gpt-4o-mini",
+    messages=message_text,
+    temperature=1,
+    max_tokens=800,
+    top_p=0.95,
+    frequency_penalty=0,
+    presence_penalty=0,
+    stop=None)
     return completion
 
 print("Welcome! How can I help you today?")
 while True:
     user_message = input(">>> ")
     completion = ai_chat(user_message)
-    print(completion['choices'][0]['message']['content'])
+    print(completion.choices[0].message.content)
